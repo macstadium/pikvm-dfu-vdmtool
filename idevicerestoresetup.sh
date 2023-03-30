1910465ab7ca59decd63e306058d29d3f3fbb509
tperfitt <tperfitt@twocanoes.com>
2023-03-29 21:10:49 -0500

    added branch

--- idevicerestoresetup.sh
+++ idevicerestoresetup.sh
@@ -1,27 +1,40 @@
 #!/bin/bash 
 
-set -e 
-apt-get update
-apt-get install -y cmake libtool autoconf automake libreadline-dev python-dev libusb-1.0-0-dev libzip-dev zlib1g-dev libssl-dev libcurl4-openssl-dev git exfat-fuse exfat-utils
+#set -e 
+#apt-get update
+#apt-get install -y cmake libtool autoconf automake libreadline-dev python-dev libusb-1.0-0-dev libzip-dev zlib1g-dev libssl-dev libcurl4-openssl-dev git exfat-fuse exfat-utils
 
-
-projects=("libplist" "libimobiledevice-glue" "libusbmuxd" "libimobiledevice" "libirecovery" "idevicerestore" "usbmuxd")
-
-for proj in ${projects[@]}; do
-
-	url="https://github.com/libimobiledevice/${proj}.git"
+function makeproject () {
+	
+	github_account="$1"
+	proj="$2"
+	branch="$3"
+	url="https://github.com/${github_account}/${proj}.git"  $branch
 	echo "cloning $url"
 	
 	git clone "${url}"
 	cd "${proj}"
+	git checkout $branch 
+
 	./autogen.sh
 	make
 	make install
 	
 	cd ..
+		
 
+}
 
+projects=("libplist" "libimobiledevice-glue" "libusbmuxd" "libimobiledevice" "libirecovery")
+
+for proj in ${projects[@]}; do
+	makeproject libimobiledevice "${proj}" master 
 done
+
+makeproject twocanoes idevicerestore  usb3
+makeproject libimobiledevice usbmuxd  master 
+
+
 useradd usbmux
 
 ldconfig
