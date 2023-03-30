81b754c90e19cc2d7cfe9bba00be336d034d7a5b
tperfitt <tperfitt@twocanoes.com>
2023-03-30 14:59:21 -0500

    uncommented needed commands

--- idevicerestoresetup.sh
+++ idevicerestoresetup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
-#set -e 
-#apt-get update
-#apt-get install -y cmake libtool autoconf automake libreadline-dev python-dev libusb-1.0-0-dev libzip-dev zlib1g-dev libssl-dev libcurl4-openssl-dev git exfat-fuse exfat-utils
+set -e 
+apt-get update
+apt-get install -y cmake libtool autoconf automake libreadline-dev python-dev libusb-1.0-0-dev libzip-dev zlib1g-dev libssl-dev libcurl4-openssl-dev git exfat-fuse exfat-utils
 
 function makeproject () {
 	
